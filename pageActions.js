382216039c9e0148ddbde982c2251c81c8a84b2f
Mohammad Bayat <m21newyork@gmail.com>
2025-06-29 12:46:42 -0400

    Adding stuff

--- pageActions.js
+++ pageActions.js
@@ -1,2 +1,16 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
+
+const a= document.getElementsByClassName('list-item-x');
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", function () {
+    const item_list= document.querySelector('.list-item-x');
+    if item_list.attributes.display === None {
+        
+    }
+
+  });
+}
